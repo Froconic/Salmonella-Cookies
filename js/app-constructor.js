8a07d7ae1b23c2e8495ef9f0d12a87bb8302341e
Froconic <froconicdesign@gmail.com>
2019-07-22 10:32:34 -0500

    Fininhed preliminary copy for the page necxt is styling and debugging the event handler

--- js/app-constructor.js
+++ js/app-constructor.js
@@ -5,7 +5,6 @@
 var hours = ['6:00', '7:00', '8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00'];
 var cookieArray = [];
 var hourlyTotals = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var form = document.querySelector('form');
 
 
 
@@ -300,21 +299,21 @@ function inputData() {
   event.preventDefault();
 
   var locationName = event.target.name.value;
-  var minimumCustomers = Number(event.target.minCustomers.value);
-  var maximumCustomers = Number(event.target.maxCustomers.value);
-  var averageSales = parseFloat(event.target.avgCookies.value);
+  var minimumCustomers = event.target.minCustomers.value;
+  var maximumCustomers = event.target.maxCustomers.value;
+  var averageSales = event.target.avgCookies.value;
   console.log('data collected');
 
   var newShop = new CookieShop(locationName, maximumCustomers, minimumCustomers, averageSales);
   console.log('data input');
 
 footerClear();
-
+finalrender();
 }
 // // ______________________EXECUTABLE___________________________________________
 
 
-
+var form = document.querySelector('form');
 form.addEventListener('submit', inputData);
 tableHeadCreator();
 finalRender();
